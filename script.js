34eeeddd8f907226cff6dd1593ae0bdd8df90e0c
Liliane <liliane.lima23@gmail.com>
2023-10-09 19:23:29 -0300

    Identação

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-function toggleMode ()
-{
+function toggleMode() {
   const html = document.documentElement
 
-  if(html.classList.contains("dark"))
-  { html.classList. remove("dark")
-  
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark")
   } else {
     html.classList.add("dark")
   }
- const img = document.querySelector("#profile img")
 
- if(html.classList.contains("dark"))
-  { img.setAttribute("src","./assets/avatar.png")
-  
-  } else {
+  const img = document.querySelector("#profile img")
+
+ if (html.classList.contains("dark")) {
+   img.setAttribute("src","./assets/avatar.png")
+ } else {
    img.setAttribute ("src","./assets/avatar-light.png")
-
   }
 
+  
 }
